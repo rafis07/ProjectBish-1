adc1c7a6ede7d4890316b958ccc3bfb0925e944f
BianSepang <bianconneri46@gmail.com>
2020-08-23 18:17:35 +0000

    pylint: auto fixes
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -13,8 +13,7 @@ from random import randint
 
 from telethon.events import StopPropagation
 
-from userbot import (AFKREASON, COUNT_MSG, CMD_HELP, ISAFK, BOTLOG,
-                     BOTLOG_CHATID, USERS, PM_AUTO_BAN)
+from userbot import (AFKREASON, CMD_HELP, BOTLOG, BOTLOG_CHATID, PM_AUTO_BAN)
 from userbot.events import register
 
 global USER_AFK  # pylint:disable=E0602
