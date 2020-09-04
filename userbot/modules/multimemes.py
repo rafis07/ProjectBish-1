0778fa5e7f5616b538961c9271c714ee01b03420
aidilaryanto <aidilaryanto18@gmail.com>
2020-09-04 10:11:57 +0800

    multimemes: sg: add indent on result
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/multimemes.py
+++ userbot/modules/multimemes.py
@@ -339,7 +339,7 @@ async def lastname(steal):
                 return
             if r.text.startswith("Name"):
                 respond = await conv.get_response()
-                await steal.edit(f"{r.message}")
+                await steal.edit(f"`{r.message}`")
                 await steal.client.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id, respond.id]
                 )
@@ -354,7 +354,7 @@ async def lastname(steal):
                 return
             else:
                 respond = await conv.get_response()
-                await steal.edit(f"{response.message}")
+                await steal.edit(f"`{response.message}`")
             await steal.client.delete_messages(
                 conv.chat_id, [msg.id, r.id, response.id, respond.id]
             )
